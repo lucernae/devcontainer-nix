542f718ec08ba2c824ff00c3aaf04ff7fb883c05
lana.pcfre@gmail.com <lana.pcfre@gmail.com>
2022-07-10 18:24:22 +0000

    change: allow docker from docker

--- default.nix
+++ default.nix
@@ -14,11 +14,6 @@ stdenv.mkDerivation rec {
 		stdenv.cc.cc.lib
 	];
 	dontBuild = true;
-	installPhase = ''
-		mkdir -p $out/lib
-		# libstdc++.so.6 is needed by vscode-server's nodejs
-		cp "${stdenv.cc.cc.lib}/lib64/libstdc++.so.6" $out/lib
-	'';
 	meta = {
 		description = "VS Code devcontainer with Nix";
 		maintainers = [ "Rizky Maulana Nugraha <lana.pcfre@gmail.com>" ];
