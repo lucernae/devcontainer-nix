9baa7b2e066a2640ae0f026e198ce1d521ec22f9
Rizky Maulana Nugraha <lana.pcfre@gmail.com>
2022-11-01 22:55:22 +0000

    format: nix file

--- default.nix
+++ default.nix
@@ -1,22 +1,22 @@
-{pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> { } }:
 with pkgs;
 stdenv.mkDerivation rec {
-	pname = "devcontainer";
-	version = "1.0";
-	src = ./.;
-	propagatedBuildInputs = [
-		makeWrapper
-		# needed for vscode-server
-		nodejs
-		gawk
-		vim
-		git
-		stdenv.cc.cc.lib
-	];
-	dontBuild = true;
-	dontInstall = true;
-	meta = {
-		description = "VS Code devcontainer with Nix";
-		maintainers = [ "Rizky Maulana Nugraha <lana.pcfre@gmail.com>" ];
-	};
+  pname = "devcontainer";
+  version = "1.0";
+  src = ./.;
+  propagatedBuildInputs = [
+    makeWrapper
+    # needed for vscode-server
+    nodejs
+    gawk
+    vim
+    git
+    stdenv.cc.cc.lib
+  ];
+  dontBuild = true;
+  dontInstall = true;
+  meta = {
+    description = "VS Code devcontainer with Nix";
+    maintainers = [ "Rizky Maulana Nugraha <lana.pcfre@gmail.com>" ];
+  };
 }
