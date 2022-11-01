00297437960c3e6afd1f8b6184c461ef707e06ac
Rizky Maulana Nugraha <lana.pcfre@gmail.com>
2022-11-01 22:55:22 +0000

    format: nix file

--- shell.nix
+++ shell.nix
@@ -1,19 +1,21 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> { } }:
 let
-	local-shell = if builtins.pathExists ./local-shell.nix then (import ./local-shell.nix {
-			inherit pkgs;
-		}) else null;
+  local-shell =
+    if builtins.pathExists ./local-shell.nix then
+      (import ./local-shell.nix {
+        inherit pkgs;
+      }) else null;
 in
 with pkgs;
 mkShell {
-	inputsFrom = [ (import ./default.nix {}) ] ++ lib.optionals (! isNull local-shell) [ local-shell ];
-	shellHook = ''
-		echo ""
-		echo "---------------------------------------------------------------"
-		echo "You are using default Nix-shell in this project."
-		echo "Use: \"direnv status\" to check if you are using correct direnv"
-		echo "Use: \"printenv\" to print current environment variable"
-		echo "---------------------------------------------------------------"
-		echo ""
-	'';
+  inputsFrom = [ (import ./default.nix { }) ] ++ lib.optionals (! isNull local-shell) [ local-shell ];
+  shellHook = ''
+    echo ""
+    echo "---------------------------------------------------------------"
+    echo "You are using default Nix-shell in this project."
+    echo "Use: \"direnv status\" to check if you are using correct direnv"
+    echo "Use: \"printenv\" to print current environment variable"
+    echo "---------------------------------------------------------------"
+    echo ""
+  '';
 }
