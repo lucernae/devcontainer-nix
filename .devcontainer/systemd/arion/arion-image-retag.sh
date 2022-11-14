07f775c00d3a0890bab9b9cb368c53ad1d05ad48
Rizky Maulana Nugraha <lana.pcfre@gmail.com>
2022-11-15 04:39:38 +0700

    refactor: project structure

--- .devcontainer/systemd/arion/arion-image-retag.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env nix-shell
-#!nix-shell -i bash -p yq jq
-
-arion_tag=$(arion config | yq ".services.devcontainer.image" -r)
-
-docker tag $arion_tag ghcr.io/lucernae/devcontainer-nix:nixos-arion
--- .devcontainer/systemd/arion/arion-post-create-command.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-docker-compose exec -T devcontainer bash -c "systemctl start nix-daemon && systemctl start dbus && nix-channel --update"
