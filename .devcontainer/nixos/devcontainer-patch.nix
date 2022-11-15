5681327090af11200a827ef1bad8f78230214659
Rizky Maulana Nugraha <lana.pcfre@gmail.com>
2022-11-15 21:05:27 +0000

    update: fix linking

--- .devcontainer/nixos/devcontainer-patch.nix
+++ .devcontainer/nixos/devcontainer-patch.nix
@@ -13,7 +13,7 @@ stdenv.mkDerivation rec {
     ln -s "${stdenv.cc.cc.lib}/lib64/libstdc++.so.6" $out/lib
     # ld-linux-x86-64.so.2 is needed by vscode-server's nodejs in case it install 32 bit nodejs
     ln -s "${glibc}/lib64/ld-linux-x86-64.so.2" $out/lib64
-    ln -s $out/lib64/ld-linux-x86-64.so.2 $out/lib64/ld-linux.so.2
+    ln -s "${glibc}/lib64/ld-linux-x86-64.so.2" $out/lib64/ld-linux.so.2
   '';
   meta = {
     description = "VS Code devcontainer with Nix";
