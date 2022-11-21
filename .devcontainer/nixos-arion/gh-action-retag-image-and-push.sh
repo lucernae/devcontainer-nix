1f41d505729c30299a4520c08713b4f5d94908e6
Rizky Maulana Nugraha <lana.pcfre@gmail.com>
2022-11-22 03:48:28 +0700

    add: debug log for image tag

--- .devcontainer/nixos-arion/gh-action-retag-image-and-push.sh
+++ .devcontainer/nixos-arion/gh-action-retag-image-and-push.sh
@@ -2,8 +2,11 @@
 # tag a source image into a multiple docker tag
 # tags from GH action are separated by comma
 
+echo "input tags: $1"
+
 for tag in ${1//,/ }
 do
+    echo "processing tag: $tag"
     docker tag ghcr.io/lucernae/devcontainer-nix:nixos-arion $tag
     docker push $tag
 done
