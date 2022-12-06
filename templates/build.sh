27d73ac12ac01ddbd5dba29bf419e3134ab4a6b6
Rizky Maulana Nugraha <lana.pcfre@gmail.com>
2022-12-07 03:47:33 +0700

    update: refactor github actions structure

--- templates/build.sh
+++ templates/build.sh
@@ -2,9 +2,10 @@
 
 srcDir="$(pwd)/src"
 testDir="$(pwd)/test"
-ghActionDir="$(pwd)/../.github/smoke-test"
+ghActionDir="$(pwd)/../.github/actions/smoke-test"
 
 templateID="$1"
 testID="$2"
 
+cd $PROJECT_DIR
 bash $ghActionDir/build.sh $templateID $testDir/$testID
