f3bec7216778e99e8973e3002828383438a7e91f
Rizky Maulana Nugraha <lana.pcfre@gmail.com>
2022-12-07 03:42:04 +0700

    update: test case

--- templates/test/nix-use-direnv/tests/test.sh
+++ templates/test/nix-use-direnv/tests/test.sh
@@ -4,7 +4,7 @@ source test-project/test-utils.sh
 
 # Template specific tests
 check "direnv status" direnv status
-check "default package installed" nix-shell --run hello
+check "default package installed" bash -c 'source ~/.bashrc; hello'
 check "direnv executed" nix-shell --run  'if [ "$MY_HOOK" == "true" ]; then echo "$MY_HOOK"; else exit 1; fi'
 
 # Report result
